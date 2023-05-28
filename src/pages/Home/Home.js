63cc31daa214a062caecd1030d5413cc572d2eea
Matt Wheeler <mtwheeler@usfca.edu>
2023-05-27 22:22:00 -0700

    Updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container, Row, Col} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import DynamicTyping from "../../components/DynamicTyping";
 import About from "../../components/About/About";
 import Timeline from "../../components/Timeline/Timeline";
